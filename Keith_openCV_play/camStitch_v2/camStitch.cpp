d7012ef71c20f5402418f484825577fa35b04baa
stribyw <stribyw@oregonstate.edu>
2018-03-08 12:17:28 -0800

    Edited and Added a program
    I edited my two-camera stitch to mirror Shu-Ping's since it's the quickest currently. Also added openCVBuildInfo directory to output our system's current build information.

--- Keith_openCV_play/camStitch_v2/camStitch.cpp
+++ Keith_openCV_play/camStitch_v2/camStitch.cpp
@@ -6,33 +6,30 @@
 using namespace std;
 using namespace cv;
 
+Stitcher::Mode mode = Stitcher::PANORAMA;
 
 int streaming(VideoCapture cap1, VideoCapture cap2)
 {
-    Mat fr1, fr2, copy1, copy2, pano;
-    bool try_use_gpu = false;
-    vector<Mat> imgs;
+	Mat fr1, fr2, copy1, copy2, pano;
+    	bool try_use_gpu = true;
+    	vector<Mat> imgs;
 
 	//while (true)
     	//{
         	cap1 >> fr1;
        		cap2 >> fr2;
-        	fr1.copyTo(copy1);
-        	fr2.copyTo(copy2);
+        	//fr1.copyTo(copy1);
+        	//fr2.copyTo(copy2);
 			
-		imgs.push_back(copy1);
-		imgs.push_back(copy2);
+		//imgs.push_back(copy1);
+		//imgs.push_back(copy2);
+		imgs.push_back(fr1);
+		imgs.push_back(fr2);
 
-        	cap1 >> fr1;
-       		cap2 >> fr2;
-       		fr1.copyTo(copy1);
-        	fr2.copyTo(copy2);
-
-		imgs.push_back(copy1);
-		imgs.push_back(copy2);
-
-        	Stitcher test = Stitcher::createDefault(try_use_gpu);
-        	Stitcher::Status status = test.stitch(imgs, pano);
+        	//Stitcher test = Stitcher::createDefault(try_use_gpu);
+        	Ptr<Stitcher> test = Stitcher::create(mode, try_use_gpu);
+		//Stitcher::Status status = test.stitch(imgs, pano);
+		Stitcher::Status status = test->stitch(imgs, pano);
 
         	if (status != Stitcher::OK)
         	{
@@ -41,20 +38,20 @@ int streaming(VideoCapture cap1, VideoCapture cap2)
         	}
 
         	
-		imshow("Frame 1", fr1);
-        	imshow("Frame 2", fr2);
+		//imshow("Frame 1", fr1);
+        	//imshow("Frame 2", fr2);
        		imshow("Stitched Image", pano);
 
-        	if(waitKey(30) >= 0) 
+        	if(waitKey(1) >= 0) 
             		return -1;
 	//}
 }
 
 int main(int argc, char *argv[])
 {
-    Mat fr1, fr2, copy1, copy2, pano;
-    bool try_use_gpu = false;
-    vector<Mat> imgs;
+    //Mat fr1, fr2, copy1, copy2, pano;
+    //bool try_use_gpu = false;
+    //vector<Mat> imgs;
     //VideoCapture cap1(), cap2();
 
 	VideoCapture cap1("nvcamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink", CAP_GSTREAMER); //middle output
