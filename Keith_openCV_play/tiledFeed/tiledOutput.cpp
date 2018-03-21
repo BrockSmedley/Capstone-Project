bd113530fb0d6d268f9fb739fbd65c90ce097913
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-03-21 01:19:24 -0700

    BGRx not directly supported by videoconvert
    see here: https://github.com/opencv/opencv/issues/11059#issuecomment-372356546

--- Keith_openCV_play/tiledFeed/tiledOutput.cpp
+++ Keith_openCV_play/tiledFeed/tiledOutput.cpp
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
 {
     Mat fr1, fr2, tile;
 
-	VideoCapture cap1("nvcamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw(memory:NVMM), format=(string)BGRx ! videoconvert ! video/x-raw(memory:NVMM), format=(string)BGR ! appsink", CAP_GSTREAMER); //middle output
+	VideoCapture cap1("nvcamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw(memory:NVMM), format=(string)BGR ! videoconvert ! video/x-raw(memory:NVMM), format=(string)BGR ! appsink", CAP_GSTREAMER); //middle output
 
-	VideoCapture cap2("nvcamerasrc sensor-id=2 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw(memory:NVMM), format=(string)BGRx ! videoconvert ! video/x-raw(memory:NVMM), format=(string)BGR ! appsink", CAP_GSTREAMER); //output closest to center of board
+	VideoCapture cap2("nvcamerasrc sensor-id=2 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw(memory:NVMM), format=(string)BGR ! videoconvert ! video/x-raw(memory:NVMM), format=(string)BGR ! appsink", CAP_GSTREAMER); //output closest to center of board
 
 	if(!cap1.isOpened()){
 		cout << "connection cap1 failed" << endl;
